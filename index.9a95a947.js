ea21fa3c1cc73718bdc0eb6c5d0317a0b36b3537
TiFAu <turchin.anton2@gmail.com>
2026-03-02 21:31:35 +0200

    make build

---
+++ index.9a95a947.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall"),e=document.querySelector(".spider"),l=t.clientWidth,i=t.clientHeight,c=e.clientWidth,o=e.clientHeight;t.style.position="relative",e.style.position="absolute",e.style.left="".concat((l-c)/2,"px"),e.style.top="".concat((i-o)/2,"px");
+//# sourceMappingURL=index.9a95a947.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+const spiderWidth = spider.clientWidth;
+const spiderHeight = spider.clientHeight;
+wall.style.position = "relative";
+spider.style.position = "absolute";
+spider.style.left = `${(wallWidth - spiderWidth) / 2}px`;
+spider.style.top = `${(wallHeight - spiderHeight) / 2}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
